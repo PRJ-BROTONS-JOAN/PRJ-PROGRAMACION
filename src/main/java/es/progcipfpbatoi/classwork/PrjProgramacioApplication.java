02242887688f2c987811621c6f59fae58881b948
Joan Brotons <joanbv2006@gmail.com>
2025-05-27 15:53:21 +0200

    [ADD] Mi primer commit

---
+++ src/main/java/es/progcipfpbatoi/classwork/PrjProgramacioApplication.java
@@ -0,0 +1,13 @@
+package es.progcipfpbatoi.classwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjProgramacioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjProgramacioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/progcipfpbatoi/classwork/PrjProgramacioApplicationTests.java
@@ -0,0 +1,13 @@
+package es.progcipfpbatoi.classwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjProgramacioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
